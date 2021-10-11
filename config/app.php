f61d5a899cae8fd473ad2a5e1e11d1f89ee024ce
Mihai <dughilamihai@gmail.com>
2021-10-12 01:57:06 +0300

    installed laravel review ratable

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Codebyray\ReviewRateable\ReviewRateableServiceProvider::class,
 
         /*
          * Package Service Providers...
